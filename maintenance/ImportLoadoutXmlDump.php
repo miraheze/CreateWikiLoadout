6f41cfb5819a32f321cfa57b6faaa71a8155eed2
Peter Li <lihaohong6@gmail.com>
2026-08-28 05:58:09 -0400

    Add more logger invocations for better timing tracking

--- maintenance/ImportLoadoutXmlDump.php
+++ maintenance/ImportLoadoutXmlDump.php
@@ -13,12 +13,15 @@ use MediaWiki\Permissions\UltimateAuthority;
 use MediaWiki\SiteStats\SiteStatsInit;
 use MediaWiki\Title\Title;
 use MediaWiki\User\User;
+use Psr\Log\LoggerInterface;
 use RebuildTextIndex;
 use RefreshLinks;
 use Throwable;
 use Wikimedia\Rdbms\IDBAccessObject;
 
 class ImportLoadoutXmlDump extends Maintenance {
+
+	private LoggerInterface $logger;
 
 	public function __construct() {
 		parent::__construct();
@@ -30,6 +33,9 @@ class ImportLoadoutXmlDump extends Maintenance {
 	}
 
 	public function execute(): void {
+		$this->logger = LoggerFactory::getInstance( 'CreateWiki' );
+		$this->logger->info( 'CreateWikiLoadout import started.' );
+
 		$xmlPath = $this->getOption( 'xml' );
 		$services = $this->getServiceContainer();
 
@@ -48,6 +54,8 @@ class ImportLoadoutXmlDump extends Maintenance {
 		try {
 			$user = User::newSystemUser( 'Maintenance script', [ 'steal' => true ] );
 			$this->deleteDefaultMainPage( $user );
+			$this->logger->info( 'CreateWikiLoadout deleted the old main page.' );
+
 			$importer = $services->getWikiImporterFactory()->getWikiImporter(
 				$importStreamSource->value,
 				new UltimateAuthority( $user )
@@ -56,42 +64,46 @@ class ImportLoadoutXmlDump extends Maintenance {
 			$importer->disableStatisticsUpdate();
 			$importer->setNoUpdates( true );
 			// assignKnownUsers is always useless because there will only be a single user in the XML dump
-			$importer->setUsernamePrefix( '', true );
+			// $importer->setUsernamePrefix( '', true );
 
 			$importer->doImport();
+			$this->logger->info( 'CreateWikiLoadout finished importing the XML dump.' );
 
 			$siteStatsInit = new SiteStatsInit();
 			$siteStatsInit->refresh();
 
 			SiteStatsUpdate::cacheUpdate( $dbw );
+			$this->logger->info( 'CreateWikiLoadout finished updateSiteStats.' );
 
 			$maintenance = new FakeMaintenance;
 			$rebuildText = $maintenance->createChild( RebuildTextIndex::class );
 			$rebuildText->execute();
+			$this->logger->info( 'CreateWikiLoadout finished rebuildTextIndex.' );
 
 			$rebuildLinks = $maintenance->createChild( RefreshLinks::class );
 			$rebuildLinks->execute();
+			$this->logger->info( 'CreateWikiLoadout finished refreshLinks.' );
 		} catch ( Throwable $t ) {
 			MWExceptionHandler::rollbackPrimaryChangesAndLog( $t );
 			$this->fatalError( $t->getMessage() );
 		}
+
+		$this->logger->info( 'CreateWikiLoadout import finished.' );
 	}
 
 	private function deleteDefaultMainPage( User $user ): void {
 		$services = $this->getServiceContainer();
-		$logger = LoggerFactory::getInstance( 'CreateWiki' );
 
 		$page = $services->getWikiPageFactory()->newFromTitle( Title::newMainPage() );
 		$page->loadPageData( IDBAccessObject::READ_LATEST );
 		if ( !$page->exists() ) {
-			$logger->warning( 'CreateWikiLoadout expected a default main page to exist but there was none.' );
+			$this->logger->warning( 'CreateWikiLoadout expected a default main page to exist but there was none.' );
 			return;
 		}
 
 		$services->getDeletePageFactory()->newDeletePage( $page, new UltimateAuthority( $user ) )
 			->forceImmediate( true )
 			->deleteUnsafe( 'Delete default main page to make way for CreateWikiLoadout import.' );
-		$logger->info( 'Default main page deleted by CreateWikiLoadout.' );
 	}
 }
 
